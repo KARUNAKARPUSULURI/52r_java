863fbc7f2670121c250e3ca633e174f338ea7f98
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-06-18 15:32:29 +0530

    explanation of code

---
+++ intro2/Details.java
@@ -0,0 +1,8 @@
+class Details{
+	public static void main(String [] args){
+		System.out.println("Name : Karunakar Pusuluri");
+		System.out.println("Age : 30");
+		System.out.println("Phone Number : 987654289");
+		System.out.println("Email : karun@gmail.com");
+	}
+}
---
+++ intro2/FirstProgram.java
@@ -0,0 +1,20 @@
+class FirstProgram{
+	public static void main(String [] args){
+		FirstProgram hello = new FirstProgram();
+		System.out.println("Hello");
+		System.out.println("Good Morning");
+		System.out.println("This is my 1st java class");
+		System.out.println(10 * 20);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
